86abb1eaf0b86d641d75addbaa370385870648a2
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-04-05 14:49:08 +0200

    Ajout de la classe télécommande

---
+++ domotique/src/main/java/Telecommande.java
@@ -0,0 +1,29 @@
+public class Telecommande {
+
+    private Lampe[] lampes;
+
+    public Telecommande(Lampe[] lampes) {
+        throw new Error("Méthode non écrite");
+    }
+
+    public void ajouterLampe(Lampe[] l){
+        throw new Error("Méthode non écrite");
+    }
+
+    public void activerLampe(int x){
+        throw new Error("Méthode non écrite");
+    }
+
+    public void desactiverLampe(int x){
+        throw new Error("Méthode non écrite");
+    }
+
+    public void activerTout(){
+        throw new Error("Méthode non écrite");
+    }
+
+    public String toString(){
+        throw new Error("Méthode non écrite");
+    }
+
+}
