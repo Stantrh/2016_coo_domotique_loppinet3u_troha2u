a7dba81fac15c1fcd4c2f5aed12681c732ce1d7f
Stephane Loppinet <stephaneloppinet@gmail.com>
2023-04-05 14:58:50 +0200

    Enlever les fichiers inutiles

--- domotique/src/test/java/TelecommandeTest.java
+++ domotique/src/test/java/TelecommandeTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class TelecommandeTest {
+public class TelecommandeTest {
+    /*
+    * ajout d'une lampe à une télécommande vide ;
+    */
+    @Test
+    public void testAjoutLampeTelecommandeVide(){
+        // préparation des données
+
+        // méthode testée
+
+        // vérification
+    }
+
+    /*
+    * ajout d'une lampe à une télécommande avec 1 élément ;
+    */
+
+    /*
+    * activation d'une lampe existante en position 0 ;
+    */
+
+    /*
+    * activation d'une lampe existante en position 1 ;
+    */
+
+    /*
+    * activation d'une lampe inexistante.
+    */
 }
