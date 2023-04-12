53a805a9d0cc89aa117cb0de6446686ad4c1d731
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2023-04-12 14:36:57 +0200

    Ajout de la classe qui permet d'adapter Cheminee en un appareil electronique

---
+++ domotique/src/main/java/AdapterCheminee.java
@@ -0,0 +1,22 @@
+public class AdapterCheminee implements Appareil{
+
+    private Cheminee cheminee;
+
+    public AdapterCheminee(Cheminee c) {
+        this.cheminee = c;
+    }
+
+
+    public void eteindre(){
+        this.cheminee.changerIntensite(0);
+    }
+
+    public void allumer(){
+        int i = this.cheminee.getLumiere();
+        this.cheminee.changerIntensite(i + 10);
+    }
+
+    public String toString(){
+        return this.cheminee.toString();
+    }
+}
